4ce00306e7cec95cad8ce5b1b95a5d9643ad4428
Jake <royalcows9@gmail.com>
2020-11-30 23:12:22 -0800

    Alright it just works with sent messages, can't figure out messages being edited
    Some of the other code that I deleted: https://discord.com/channels/690803297988116490/775640493890732062/783224758195847178

--- plugins/CopyMoji/index.js
+++ plugins/CopyMoji/index.js
@@ -1,8 +1,8 @@
 module.exports = (Plugin, Library) => {
 
-  const {Logger, Patcher, Settings, DOMTools} = Library;
+  const {Logger, Patcher, Settings, DOMTools, ReactTools} = Library;
 
-  return class ExamplePlugin extends Plugin {
+  return class CopyMoji extends Plugin {
     constructor() {
       super();
       this.defaultSettings = {};
@@ -39,30 +39,31 @@ module.exports = (Plugin, Library) => {
     }
 
     onEmojiRender(event) {
+      console.log("thing was ran");
       try {
         // If the update is not of type childList, then we don't care
         event.forEach(mutationRecord => {
-          if (mutationRecord.type !== "childList") {
-            return;
-          }
-          if (mutationRecord.addedNodes.length === 0) {
-            Logger.log("No emojis added");
-          }
-
-          mutationRecord.addedNodes.forEach(addedNode => {
-            // If node isn't an element, then we don't care.
-            if (addedNode.nodeType !== 1) {
-              return;
+          if (mutationRecord.type === "childList") {
+            if (mutationRecord.addedNodes.length === 0) {
+              Logger.log("No emojis added");
             }
-            let emojis = DOMTools.queryAll("img.emoji", addedNode.parentElement);
-            emojis.forEach(emoji => {
-              // If an emoji is nonstandard, return because we can't do anything about that.
-              if (!this.checkEmojiIsStandard(emoji.alt.replace(/:/g, ""))) {
+
+            mutationRecord.addedNodes.forEach(addedNode => {
+              // If node isn't an element, then we don't care.
+              if (addedNode.nodeType !== 1) {
                 return;
               }
-              emoji.alt = this.emojiLookup[emoji.alt.replace(/:/g, "")];
+              let emojis = DOMTools.queryAll("img.emoji", addedNode.parentElement);
+              emojis.forEach(emoji => {
+                // If an emoji is nonstandard, return because we can't do anything about that.
+                if (!this.checkEmojiIsStandard(emoji.alt.replace(/:/g, ""))) {
+                  return;
+                }
+
+                emoji.alt = this.emojiLookup[emoji.alt.replace(/:/g, "")];
+              });
             });
-          });
+          }
         });
       } catch (e) {
         Logger.err(e);
@@ -84,91 +85,8 @@ module.exports = (Plugin, Library) => {
 
     getSettingsPanel() {
       return Settings.SettingPanel.build(this.saveSettings.bind(this),
-        new Settings.SettingGroup("Example Plugin Settings").append(
-          new Settings.Textbox("Textbox", "This should be a description of what this setting is about or blank", this.settings.textbox, (e) => {
-            this.settings.textbox = e;
-          }),
-          new Settings.Dropdown("Select", "This should be a description of what this setting is about or blank", this.settings.option, [
-            {
-              label: "Test 1",
-              value: "weiner"
-            },
-            {
-              label: "Test 2",
-              value: 50
-            },
-            {
-              label: "Test 3",
-              value: JSON.stringify({
-                label: "Test 1",
-                value: "weiner"
-              })
-            },
-          ], (e) => {
-            this.settings.option = e;
-          }),
-          new Settings.SettingGroup("Example Plugin SubSettings", {shown: true}).append(
-            new Settings.RadioGroup("Generic RadioGroup", "This should be a description of what this setting is about or blank", this.settings.radio, [
-              {
-                name: "Test 1",
-                value: "weiner",
-                desc: "This is the first test",
-                color: "#ff0000"
-              },
-              {
-                name: "Test 2",
-                value: 50,
-                desc: "This is the second test",
-                color: "#00ff00"
-              },
-              {
-                name: "Test 3",
-                value: JSON.stringify({
-                  label: "Test 1",
-                  value: "weiner"
-                }),
-                desc: "This is the third test",
-                color: "#0000ff"
-              },
-            ], (e) => {
-              this.settings.radio = e;
-            }),
-            new Settings.Switch("Switch1", "This should be a description of what this setting is about or blank", this.settings.switch1, (e) => {
-              this.settings.switch1 = e;
-            }),
-            new Settings.Switch("Switch2", "This should be a description of what this setting is about or blank", this.settings.switch2, (e) => {
-              this.settings.switch2 = e;
-            }),
-            new Settings.Switch("Switch3", "This should be a description of what this setting is about or blank", this.settings.switch3, (e) => {
-              this.settings.switch3 = e;
-            }),
-            new Settings.Switch("Switch4", "This should be a description of what this setting is about or blank", this.settings.switch4, (e) => {
-              this.settings.switch4 = e;
-            })
-          )
-        ),
-
-        new Settings.SettingGroup("Example Advanced Settings").append(
-          new Settings.ColorPicker("Color Example", "This should be a description of what this setting is about or blank", this.settings.color, (e) => {
-            this.settings.color = e;
-          }),
-          new Settings.Keybind("Keybind", "This should be a description of what this setting is about or blank", this.settings.keybind, (e) => {
-            this.settings.keybind = e;
-          }),
-          new Settings.Slider("Slider", "This should be a description of what this setting is about or blank", 0, 100, this.settings.slider1, (e) => {
-            this.settings.slider1 = e;
-          }),
-          new Settings.Slider("Slider2", "This should be a description of what this setting is about or blank", 0, 90, this.settings.slider2, (e) => {
-            this.settings.slider2 = e;
-          }, {
-            markers: [0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90],
-            stickToMarkers: true
-          }),
-          new Settings.FilePicker("FilePicker", "This should be a description of what this setting is about or blank", (e) => {
-            if (e) this.settings.file = e.path;
-          })
+        new Settings.SettingGroup("There are currently no settings. Sorry!!")
         )
-      );
     }
   };
 
